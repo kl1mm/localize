ac1b0f144515bcede5ca0c21b9e04951c98aea27
Tobi Kli <tobias.klimm@gmail.com>
2021-08-02 10:13:44 +0200

    Added Example-Project

---
+++ example/kli.Localize.Example/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace kli.Localize.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Neutral/Invariant
+            Console.WriteLine(Localizations.Locale.MyText);  // Hallo Welt (German)
+
+            CultureInfo.CurrentUICulture = new CultureInfo("en");
+            Console.WriteLine(Localizations.Locale.MyText);  // Hello World (English)
+
+            CultureInfo.CurrentUICulture = new CultureInfo("en-US");
+            Console.WriteLine(Localizations.Locale.MyText);  // Hello World (English) -- Fallback to Parent.Culture
+
+            CultureInfo.CurrentUICulture = new CultureInfo("fr");
+            Console.WriteLine(Localizations.Locale.MyText);  // Hallo Welt (German) -- Fallback to Neutral
+        }
+    }
+}
