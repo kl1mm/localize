68e7770b2ae47b275894e071e0aca26a62ec181b
mMilk <tobias.klimm@gmail.com>
2021-08-02 18:00:20 +0200

    test(TranslationReaderTest): Added new Tests

---
+++ src/kli.Localize.Test/TranslationReaderTest.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using kli.Localize.Generator.Internal;
+using Microsoft.CodeAnalysis;
+using Moq;
+using Xunit;
+
+namespace kli.Localize.Test
+{
+    public class TranslationReaderTest
+    {
+        private readonly Mock<Action<Diagnostic>> reporterMock = new();
+
+        [Fact]
+        public void TestRead()
+        {
+            var filePath = Path.GetRandomFileName();
+            try
+            {
+                File.WriteAllText(filePath, "{\"Foo\": \"One\", \"Bar\": \"Two\"}");
+
+                var reader = new TranslationReader(reporterMock.Object);
+                var translations = reader.Read(Path.Combine(filePath));
+
+                Assert.Equal(2, translations.Count);
+                Assert.Equal("One", translations["Foo"]);
+                Assert.Equal("Two", translations["Bar"]);
+
+                reporterMock.Verify(m => m(It.IsAny<Diagnostic>()), Times.Never);
+            }
+            finally
+            {
+                File.Delete(filePath);
+            }
+        }
+
+        [Fact]
+        public void TestReadWarnings()
+        {
+            var filePath = Path.GetRandomFileName();
+            try
+            {
+                File.WriteAllText(filePath, "{\"1\": \"One\", \" \": \"Blank\"}");
+
+                var reader = new TranslationReader(reporterMock.Object);
+
+                Assert.Empty(reader.Read(Path.Combine(filePath)));
+                reporterMock.Verify(m => m(It.IsAny<Diagnostic>()), Times.Exactly(2));
+            }
+            finally
+            {
+                File.Delete(filePath);
+            }
+        }
+
+        [Fact]
+        public void TestReadError()
+        {
+            var filePath = Path.GetRandomFileName();
+            try
+            {
+                File.WriteAllText(filePath, "{\"ArrayValue\": [ 1, 2, 3 ]}");
+
+                var reader = new TranslationReader(reporterMock.Object);
+
+                var ex = Assert.Throws<FormatException>(() => reader.Read(Path.Combine(filePath)));
+                Assert.Contains("Only string values are allowed", ex.Message);
+            }
+            finally
+            {
+                File.Delete(filePath);
+            }
+        }
+    }
+}
