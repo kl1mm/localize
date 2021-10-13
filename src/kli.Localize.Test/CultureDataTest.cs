be41a26498c1a489596645f1a38f118c86f97da2
Florian Gilde <fgilde@gmail.com>
2021-10-13 17:46:14 +0200

    Allow dots in filenames for nesting items automatically in asp.core projects

---
+++ src/kli.Localize.Test/CultureDataTest.cs
@@ -0,0 +1,19 @@
+﻿using kli.Localize.Generator.Internal;
+using Xunit;
+
+namespace kli.Localize.Test
+{
+    public class CultureDataTest
+    {
+        [Theory]
+        [InlineData("en-US", "Locale_en-US.json")]
+        [InlineData("de-DE", "Locale_de-DE.json")]
+        [InlineData("de-DE", "Locale.de-DE.json")]
+        [InlineData("de", "Locale.de.json")]
+        [InlineData("en", "Locale.en.json")]
+        public void CanReadCultureNameFromFileName(string expected, string fileName)
+        {
+            Assert.Equal(expected, CultureData.ReadCultureNameFromFileName(fileName));
+        }
+    }
+}
